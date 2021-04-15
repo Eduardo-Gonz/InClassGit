37b1a0be6a7aac3e5255f3ee45433da54d760f19
Eduardo-Gonz <erugonz249@gmail.com>
2021-04-15 16:51:30 -0700

    create list of results

--- calc.py
+++ calc.py
@@ -1,9 +1,10 @@
 def calc(a, b):
     sum = a + b
-    print(sum)
     difference = a - b
-    print(difference)
     multiply = a * b
-    print(multiply)
     division = a / b
-    print(division)
+    results = [sum, difference, multiply, division]
+
+
+
+calc(2,3)
