130c9e23627f89ea56fc2d0c88cebeefc5d78d05
Eduardo-Gonz <erugonz249@gmail.com>
2021-04-16 22:32:54 -0700

    Prompt user for desired length of password

--- password.py
+++ password.py
@@ -2,7 +2,8 @@ def generate_password(length):
     pass
 
 def prompt_length():
-    pass
+    passLength = input("Enter length of desired password: ")
+    generate_password(passLength)
 
 
 prompt_length()
