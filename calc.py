f7dd9fb1b997dc9b98069bc2991578ef37f2872e
Eduardo-Gonz <erugonz249@gmail.com>
2021-04-16 22:03:10 -0700

    print list of results

--- calc.py
+++ calc.py
@@ -4,6 +4,11 @@ def calc(a, b):
     multiply = a * b
     division = a / b
     results = [sum, difference, multiply, division]
+    sumOfResults = 0
+    for x in results:
+        sumOfResults += x
+    
+    print(sumOfResults)
 
 
 
